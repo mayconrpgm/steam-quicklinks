c4f27e713836f0b49244056f81b39818698239c7
Maycon Queiros <maycon.queiros@nuance.com>
2017-10-27 19:45:48 -0200

    Fixing js and json file

---
+++ scripts.js
@@ -0,0 +1,57 @@
+var linksGroup = null;
+
+$(function() {
+  $.getJSON("links.json", function(data) {
+    linksGroup = data; 
+    linksGroup = sortOnKeys(linksGroup);
+
+    updateCards();
+  });
+});
+
+function updateCards(){
+  $("#cards-container").empty();
+
+  $.each(linksGroup, function(key, val) {
+    cardHTML = "<div class=\"card\">";
+    header = "";
+    
+    header = generateCardHeader(key);
+    
+    if (header !== "") {
+      cardHTML += header + "<ul class=\"list-group list-group-flush\">";
+      lines = "";
+
+      $.each(val, function(item) {
+          lines += generateCardItem(item['Link'], item['Name']);
+      });
+      cardHTML += lines + "</ul></div>";
+
+      $("#cards-container").append(cardHTML);
+    }
+  });
+}
+
+function generateCardHeader(cardName) {
+  return "<div class=\"card-header\"><h2>" + cardName + "</h2></div>";
+}
+
+function generateCardItem(itemLink, itemName) {
+  return "<li class=\"list-group-item\"><a href=\"" + itemLink + "\">" + itemName + "</a></li>";
+}
+
+function sortOnKeys(dict) {
+
+    var sorted = [];
+    for(var key in dict) {
+        sorted[sorted.length] = key;
+    }
+    sorted.sort();
+
+    var tempDict = {};
+    for(var i = 0; i < sorted.length; i++) {
+        tempDict[sorted[i]] = dict[sorted[i]];
+    }
+
+    return tempDict;
+}
